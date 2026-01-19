9448ba4644519c5a6e71e737a7a910c6d37c3a7f
Gradi Puata <gradipuata@gmail.com>
2026-01-19 10:41:11 +0200

    Set up for Identity

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,17 +5,4 @@ public class AppDbContext : DbContext
   public AppDbContext(DbContextOptions options) : base(options)
   {
   }
-  public DbSet<User> Users { get; set; }
-  public DbSet<Course> Courses { get; set; }
-
-
-  //Relationships: One user has many Courses
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    modelBuilder.Entity<Course>()
-    .HasOne(c => c.User)
-    .WithMany(u => u.Courses)
-    .HasForeignKey(u => u.UserId);
-
-  }
 }
--- Dtos/User/CreateUserDto.cs
+++
@@ -1,9 +0,0 @@
-public class CreateUserDto
-{
-  public int Id { get; set; }
-  public string? FirstName { get; set; }
-  public string? LastName { get; set; }
-  public string? Username { get; set; }
-  public int Age { get; set; }
-  public List<Course> Courses { get; set; } = new List<Course> { };
-}
