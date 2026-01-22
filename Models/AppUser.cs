2b26483da9f988919e08c74242e6a4789035b809
Gradi Puata <gradipuata@gmail.com>
2026-01-22 10:40:25 +0200

    added toktn service

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -2,7 +2,5 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 public class AppUser : IdentityUser
 {
-  [PersonalData]
-  [Column(TypeName = "nvarchar(150)")]
-  public string? FullName { get; set; }
+
 }
--- services/TokenService.cs
+++ services/TokenService.cs
@@ -1,7 +1,37 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 public class TokenService : ITokenService
 {
+  private readonly IConfiguration _config;
+  private readonly SymmetricSecurityKey _key;
+  public TokenService(IConfiguration configuration)
+  {
+    _config = configuration;
+    _key = new SymmetricSecurityKey(
+      Encoding.UTF8.GetBytes(_config["JWT:SigningKey"])
+    );
+  }
   public string CreateToken(AppUser appUser)
   {
-    throw new NotImplementedException();
+    var claims = new List<Claim>
+   {
+     new Claim(JwtRegisteredClaimNames.Email,appUser.Email),
+     new Claim(JwtRegisteredClaimNames.GivenName,appUser.UserName)
+   };
+    var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+      Subject = new ClaimsIdentity(claims),
+      Expires = DateTime.Now.AddDays(2),
+      SigningCredentials = creds,
+      Issuer = _config["JWT:Issuer"],
+      Audience = _config["JWT:Audience"]
+    };
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return tokenHandler.WriteToken(token);
   }
 }
